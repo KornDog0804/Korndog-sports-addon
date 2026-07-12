a272dc2cd17db17d6e48c94754d61820b07b6771
KornDog0804 <titans.rule1215@gmail.com>
2026-07-12 13:34:26 -0500

    Enhance catalog function with genre filtering
    Added genre filtering to channels based on query parameter.

--- netlify/functions/catalog.js
+++ netlify/functions/catalog.js
@@ -1,9 +1,15 @@
 // netlify/functions/catalog.js
 
 const { getMergedSportsChannels } = require('../../lib/sources');
+const { CATEGORY_RULES } = require('../../lib/categories');
+
+// Stremio expects the manifest to declare which genres exist so it can show
+// them as filter tabs - manifest.js needs a matching "extra" block for this
+// to actually render in the UI (see note below).
+const KNOWN_GENRES = ['Featured', ...CATEGORY_RULES.map(r => r.category)];
 
 exports.handler = async (event) => {
-  const { type, id } = event.queryStringParameters || {};
+  const { type, id, genre } = event.queryStringParameters || {};
 
   const headers = {
     'Content-Type': 'application/json',
@@ -15,7 +21,11 @@ exports.handler = async (event) => {
   }
 
   try {
-    const channels = await getMergedSportsChannels();
+    let channels = await getMergedSportsChannels();
+
+    if (genre && genre !== 'Featured') {
+      channels = channels.filter(ch => ch.category === genre);
+    }
 
     const metas = channels.map(ch => ({
       id: ch.id,
@@ -23,7 +33,8 @@ exports.handler = async (event) => {
       name: ch.name,
       poster: ch.logo,
       posterShape: 'square',
-      description: ch.description
+      description: ch.description,
+      genres: [ch.category]
     }));
 
     return { statusCode: 200, headers, body: JSON.stringify({ metas }) };
