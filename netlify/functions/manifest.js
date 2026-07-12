1b84fbdfa0f145b0f909b917d5d966670649a8e8
KornDog0804 <titans.rule1215@gmail.com>
2026-07-12 13:35:10 -0500

    Refactor manifest.js to update catalog structure

--- netlify/functions/manifest.js
+++ netlify/functions/manifest.js
@@ -1,30 +1,14 @@
-// netlify/functions/manifest.js
-
-const MANIFEST = {
-  id: 'org.korndog.sports',
-  version: '0.1.0',
-  name: 'KornDog Sports',
-  description: 'Free live sports channels from Pluto TV, Samsung TV+, Xumo and Plex, merged into one KornDog catalog.',
-  logo: 'https://korndogrecords.com/favicon.png',
-  resources: ['catalog', 'stream'],
-  types: ['tv'],
-  catalogs: [
+catalogs: [
     {
       type: 'tv',
       id: 'korndog-sports',
-      name: 'KornDog Sports'
+      name: 'KornDog Sports',
+      extra: [
+        {
+          name: 'genre',
+          options: ['Featured', 'Combat Sports', 'Motorsports', 'Football', 'Basketball', 'Baseball', 'Soccer', 'Golf', 'Tennis', 'Hunting', 'Fishing', 'Outdoor', 'Classic Sports', 'International'],
+          isRequired: false
+        }
+      ]
     }
   ],
-  idPrefixes: ['pluto-']
-};
-
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(MANIFEST)
-  };
-};
